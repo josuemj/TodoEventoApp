4aaf0edc56fe9dc612cb70ae5940d2e98a026afb
josuemj <98498772+josuemj@users.noreply.github.com>
2023-08-29 16:54:14 -0300

    Details screen finished

--- app/src/main/java/com/example/myapplication/DetailScreen.kt
+++ app/src/main/java/com/example/myapplication/DetailScreen.kt
@@ -1,18 +1,23 @@
 package com.example.myapplication
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class DetailScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +45,13 @@ fun detailsScren(){
                 .background(Color(0xFFfbd9e5))
                 .weight(1f)
         ){
-
+            Text(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(10.dp),
+                fontSize = 16.sp,
+                text = "Lugar"
+            )
         }
 
         Box(
@@ -51,19 +62,104 @@ fun detailsScren(){
         ){
 
             Column() {
-                Text(text = "Title")
+                Text(
+                    modifier = Modifier.padding(start = 10.dp, top = 10.dp, bottom = 20.dp),
+                    fontSize = 22.sp,
+                    text = "Title",
+                )
                 //Row Information
 
-                Row() {
+                Row(
+                    modifier = Modifier.height(40.dp).padding(start=10.dp)
+                ) {
                     Icon(
-                        modifier = Modifier.width(20.dp).height(20.dp),
+                        modifier = Modifier
+                            .width(30.dp)
+                            .height(30.dp)
+                            .align(Alignment.CenterVertically),
                         painter = painterResource(id = R.drawable.calendaricon),
                         contentDescription = null)
-                    Text(text = "fecha")
-                    Text(text = "Hora")
+
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        text = "Fecha",
+                        fontSize = 16.sp
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(start = 250.dp),
+                        text = "Hora",
+                        fontSize = 16.sp
+                    )
                 }
-                Text(text="About")
-                Text(text="Paragraph.....")
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(220.dp)
+                ){
+                    Column() {
+                        Text(
+                            modifier = Modifier.padding(start =12.dp, bottom = 5.dp),
+                            text="About",
+                            fontSize = 18.sp
+
+                        )
+
+                        Text(
+                            modifier = Modifier.padding(start =12.dp,end=20.dp),
+                            text="Amidst the gentle rustling of leaves " +
+                                    "and the distant chirping of birds, a " +
+                                    "sense of tranquility fills the air. The " +
+                                    "sun casts a warm golden glow, painting " +
+                                    "the sky in hues of orange and pink as it" +
+                                    " begins its slow descent. Nature seems to " +
+                                    "pause in reverence to this serene moment, " +
+                                    "creating a perfect symphony of calmness and beauty.",
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+                
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .width(170.dp)
+                            .padding(end = 5.dp)
+                            .height(40.dp),
+                        shape = RoundedCornerShape(50),
+                        colors = ButtonDefaults.buttonColors(Color(0xFFEBD0F9))
+                    ) {
+                        Text(
+                            text = "Favorite",
+                            color = Color.Black
+                        )
+                    }
+
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .width(170.dp)
+                            .padding(start = 5.dp)
+                            .height(40.dp),
+                        shape = RoundedCornerShape(50),
+                        colors = ButtonDefaults.buttonColors(Color(0xFFEBD0F9))
+                    ) {
+                        Text(
+                            text = "Favorite",
+                            color = Color.Black
+                        )
+                    }
+                }
+
             }
 
         }
